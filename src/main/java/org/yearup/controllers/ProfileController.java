c9b5c2d88af154966d6b293f546872d32432d136
Owner <fevzifd2023@gmail.com>
2025-12-17 14:00:44 -0500

    create ProfileController and add GetMapping getProfile method

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,38 @@
+package org.yearup.controllers;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class ProfileController {
+
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
+
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+
+    @GetMapping()
+    public Profile getProfile(Principal principal){
+
+        int userId=userDao.getIdByUsername(principal.getName());
+
+        return profileDao.getProfile(userId);
+    }
+
+}
